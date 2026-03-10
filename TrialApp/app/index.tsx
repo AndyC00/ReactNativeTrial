80bfa7f6c7121e67aa6fce665954e89cde98932e
AndyC00 <chenjinzhi96@gmail.com>
2026-03-10 14:58:45 +1300

    some trials

--- TrialApp/app/index.tsx
+++ TrialApp/app/index.tsx
@@ -1,15 +1,36 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, TextInput } from "react-native";
+import { Image } from "expo-image";
+
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+
+      <Text>Hello World</Text>
+
+      <Image 
+        source={{
+          uri: "https://www.shutterstock.com/image-vector/hand-drawn-cartoon-malatang-illustration-260nw-2132309287.jpg",
+        }}
+        style={styles.image}
+      />
+
+        <TextInput placeholder="Email"/>
+
     </View>
   );
 }
+
+// styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  image: {
+    width: 200,
+    height: 190
+  }
+});
