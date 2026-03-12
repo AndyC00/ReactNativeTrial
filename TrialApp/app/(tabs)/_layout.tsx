4d2ea3a46543e9eb592cbf8ddf31d67a536887d8
AndyC00 <chenjinzhi96@gmail.com>
2026-03-12 16:55:53 +1300

    try icon in expo

--- TrialApp/app/(tabs)/_layout.tsx
+++ TrialApp/app/(tabs)/_layout.tsx
@@ -1,7 +1,40 @@
 import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+
 
 export default function TabsLayout() {
-  return(
-        <Tabs></Tabs>
+    return (
+        <Tabs screenOptions={{ tabBarActiveTintColor: "coral" }}>
+            <Tabs.Screen name="index" options={{
+                title: "Home",
+                tabBarIcon: ({ color, size, focused }) => (
+                    <Ionicons 
+                        name= { focused ? "home" : "home-outline"}
+                        color={color}
+                        size={size} />
+                )
+            }}
+            />
+
+            <Tabs.Screen name="content" options={{
+                title: "Content",
+                tabBarIcon: ({ color, size, focused }) => (
+                    <Ionicons
+                        name= { focused ? "book" : "book-outline"}
+                        color={color}
+                        size={size} />
+                ),
+            }} />
+
+            <Tabs.Screen name="about" options={{
+                title: "About",
+                tabBarIcon: ({ color, size }) => (
+                    <Ionicons
+                        name= "help" 
+                        color={color}
+                        size={size} />
+                ),
+            }} />
+        </Tabs>
     );
 }
